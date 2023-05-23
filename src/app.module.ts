285f30a2a6756e0aad62714df8823aa49b84587b
thuan.le <van.thuan@blockodyssey.io>
2023-05-23 15:42:08 +0700

    remove app controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
+import { NoteModule } from './note/note.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { UserModule } from './user/user.module';
-import { NoteModule } from './note/note.module';
-import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
@@ -18,7 +16,5 @@ import { ConfigModule } from '@nestjs/config';
     NoteModule,
     ConfigModule,
   ],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
